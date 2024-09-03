df34ed95a04f5b97e6e232d024b188dc6021bacc
Abdullahi Olaiwon <olaiwonabdullahi@gmail.com>
2024-09-03 09:24:28 +0100

    update App.tsx and src/index.css

--- src/App.tsx
+++ src/App.tsx
@@ -24,11 +24,13 @@ function Form() {
 
 function PackingList() {
   return (
-    <ul className="list">
-      {initialItems.map((item) => (
-        <Item item={item} />
-      ))}
-    </ul>
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} />
+        ))}
+      </ul>
+    </div>
   );
 }
 
