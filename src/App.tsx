d3048ed593630f36d9435c5136465b7fd0770391
Abdullahi Olaiwon <olaiwonabdullahi@gmail.com>
2024-09-06 19:39:49 +0100

    update App.tsx and data.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { initialItems } from "./data.tsx";
 export default function App() {
   return (
@@ -15,10 +16,42 @@ function Logo() {
 }
 
 function Form() {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState("1");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (!description) return;
+
+    const newitem = { description, quantity, packed: false, id: Date.now() };
+
+    console.log(newitem);
+
+    setDescription("");
+    setQuantity(1);
+  }
   return (
-    <div className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>What did you need for your trip?😁</h3>
-    </div>
+      <select
+        onChange={(e) => setQuantity(Number(e.target.value))}
+        value={quantity}
+      >
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input
+        type="text"
+        placeholder="item..."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
+      <button>Add</button>
+    </form>
   );
 }
 
@@ -27,7 +60,7 @@ function PackingList() {
     <div className="list">
       <ul>
         {initialItems.map((item) => (
-          <Item item={item} />
+          <Item item={item} key={item.id} />
         ))}
       </ul>
     </div>
